1559ed4ca6a91f8d182ed8cb57f9858f6a5b4f00
malikahafez <malikamhafez@gmail.com>
2024-12-17 23:13:30 +0200

    did add to want to go list for santorini

--- myApp/app.js
+++ myApp/app.js
@@ -195,6 +195,42 @@ app.post('/bali',isAuthenticated,async function(req,res){
 app.get('/santorini',isAuthenticated, function(req,res){
   res.render('santorini')
 });
+//add santorini to wanttogo list
+app.post('/santorini',isAuthenticated,async function(req,res){
+  const username = req.session.username;
+
+  try {
+    const user = await customerCollection.findOne({ username: username });
+    if(user.wanttogolist.includes('santorini')){
+      res.send(`
+        <h1>Destination already exists in your Want-to-Go List</h1>
+        <body>You can try adding another destination<body>
+        <br><br>
+        <a href="/santorini">ok</a>
+      `);
+    }
+    else{
+    user.wanttogolist.push('santorini');
+    db.collection('myCollection').updateOne(
+      {username:user.username},
+      {$set:{wanttogolist:user.wanttogolist}}, 
+      function(err, result){
+      if(err)throw err;
+    });  
+    res.send(`
+      <h1>Destination successfully added to your Want-to-Go List</h1>
+      <body><body>
+      <br><br>
+      <a href="/home">ok</a>
+    `);
+    }
+    //res.render('wanttogo', { username: username, wanttogolist: user.wanttogolist });
+    console.log(user.wanttogolist);
+  } catch (err) {
+    console.error('Error fetching Want-to-Go List:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 //go to cities page after clicking 'view'
 app.get('/cities',isAuthenticated, function(req,res){
   res.render('cities')
