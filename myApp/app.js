0dbd80b52941df2e3bab872d3a4197c75d6ee94b
malikahafez <malikamhafez@gmail.com>
2024-12-17 23:07:36 +0200

    did add to want to go for annapurna

--- myApp/app.js
+++ myApp/app.js
@@ -219,6 +219,42 @@ app.get('/inca',isAuthenticated, function(req,res){
 app.get('/annapurna',isAuthenticated, function(req,res){
   res.render('annapurna')
 });
+//add annapurna to wanttogo list
+app.post('/annapurna',isAuthenticated,async function(req,res){
+  const username = req.session.username;
+
+  try {
+    const user = await customerCollection.findOne({ username: username });
+    if(user.wanttogolist.includes('annapurna')){
+      res.send(`
+        <h1>Destination already exists in your Want-to-Go List</h1>
+        <body>You can try adding another destination<body>
+        <br><br>
+        <a href="/annapurna">ok</a>
+      `);
+    }
+    else{
+    user.wanttogolist.push('annapurna');
+    db.collection('myCollection').updateOne(
+      {username:user.username},
+      {$set:{wanttogolist:user.wanttogolist}}, 
+      function(err, result){
+      if(err)throw err;
+    });  
+    res.send(`
+      <h1>Destination successfully added to your Want-to-Go List</h1>
+      <body><body>
+      <br><br>
+      <a href="/home">ok</a>
+    `);
+    }
+    //res.render('wanttogo', { username: username, wanttogolist: user.wanttogolist });
+    console.log(user.wanttogolist);
+  } catch (err) {
+    console.error('Error fetching Want-to-Go List:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
 // app.post('/search', function(req,res){
 //   res.render('searchresults')
 // });
